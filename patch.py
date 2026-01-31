0f942c0efe0b32acf5acf149e0795c0fc558607c
BuildTools <unconfigured@null.spigotmc.org>
2026-02-01 03:37:21 +0900

    fix

--- patch.py
+++ patch.py
@@ -8,6 +8,7 @@ if (typeof(to_replace_variable) == "string") {
 }
 """.strip().replace("\n", ";").replace("    ", "").replace(";};", "}; ").replace(";}", "}").replace("{;", "{")
 
+check = []
 for folder in ["build/*.js", "build/**/*.js"]:
     for file in glob.glob(folder):
         src = open(file, "r", encoding="utf-8").read()
@@ -16,78 +17,82 @@ for folder in ["build/*.js", "build/**/*.js"]:
         src = src.replace('"/search', '"/censys')
         if src != original:
             original = src
+            check.append(1)
             print("[1] patched", file)
         src = src.replace('"https://platform.censys.io"', '"https://capi.voids.top"')
         if src != original:
             original = src
+            check.append(2)
             print("[2] patched", file)
         src = src.replace('let t=await be(i,e);', host_replacer.replace("to_replace_variable", "i") + 'let t = await be(i,e);')
         if src != original:
             original = src
+            check.append(3)
             print("[3] patched", file)
         src = src.replace('`/search', '`/censys')
         if src != original:
             original = src
-            print("[5] patched", file)
+            check.append(4)
+            print("[4] patched", file)
         src = src.replace('"path":"search"', '"path":"censys"')
         if src != original:
             original = src
-            print("[6] patched", file)
+            check.append(5)
+            print("[5] patched", file)
         src = src.replace('"routes/search"', '"routes/censys"')
         if src != original:
             original = src
-            print("[7] patched", file)
+            check.append(6)
+            print("[6] patched", file)
         src = src.replace('"routes/search._index"', '"routes/censys._index"')
         if src != original:
             original = src
-            print("[8] patched", file)
+            check.append(7)
+            print("[7] patched", file)
         src = src.replace('"routes/search.language"', '"routes/censys.language"')
         if src != original:
             original = src
-            print("[9] patched", file)
+            check.append(8)
+            print("[8] patched", file)
         src = src.replace('"routes/search.report"', '"routes/censys.report"')
         if src != original:
             original = src
-            print("[10] patched", file)
+            check.append(9)
+            print("[9] patched", file)
         src = src.replace('"routes/search.report.data"', '"routes/censys.report.data"')
         if src != original:
             original = src
-            print("[11] patched", file)
+            check.append(10)
+            print("[10] patched", file)
         src = src.replace('"routes/search.report.data.table"', '"routes/censys.report.data.table"')
         if src != original:
             original = src
-            print("[12] patched", file)
+            check.append(11)
+            print("[11] patched", file)
         src = src.replace('"routes/search.report.data.json"', '"routes/censys.report.data.json"')
         if src != original:
             original = src
+            check.append(12)
+            print("[12] patched", file)
+        src = src.replace('let g=[["q",r],["data_sets",e.join(",")]];', 'let g=[["q",((new URL(window.location.href)).searchParams.get("q") || "").trim()],["data_sets",e.join(",")]];')
+        if src != original:
+            original = src
+            check.append(13)
             print("[13] patched", file)
-        src = src.replace('throw Error(h(418));', '{}')
+        src = src.replace('defaultSuggestions:m,contextSuggestions:g,initialValue:e,', 'defaultSuggestions:m,contextSuggestions:g,initialValue:e||((new URL(window.location.href)).searchParams.get("q") || "").trim(),')
         if src != original:
             original = src
+            check.append(14)
             print("[14] patched", file)
-        src = src.replace('throw ka(),Error(h(418));', '{}')
-        if src != original:
-            original = src
-            print("[15] patched", file)
-        src = src.replace('throw Error(h(425))', '{}')
-        if src != original:
-            original = src
-            print("[16] patched", file)
-        src = src.replace('throw Error(h(425))', '{}')
-        if src != original:
-            original = src
-            print("[16] patched", file)
-        src = src.replace('let m=[["q",t],["data_sets",e.join(",")]];', 'let m=[["q",((new URL(window.location.href)).searchParams.get("q") || "").trim()],["data_sets",e.join(",")]];')
-        if src != original:
-            original = src
-            print("[17] patched", file)
-        src = src.replace('defaultSuggestions:u,contextSuggestions:S,initialValue:e,', 'defaultSuggestions:u,contextSuggestions:S,initialValue:e||((new URL(window.location.href)).searchParams.get("q") || "").trim(),')
-        if src != original:
-            original = src
-            print("[18] patched", file)
         src = src.replace('m&&(p=v(e,s,typeof m=="boolean"?"and":m)),r.set("q",p)}', 'm&&(p=v(((new URL(window.location.href)).searchParams.get("q") || "").trim(),s,typeof m=="boolean"?"and":m)),r.set("q",p)}')
         if src != original:
             original = src
-            print("[19] patched", file)
+            check.append(15)
+            print("[15] patched", file)
         if before != src:
             open(file, "w", encoding="utf-8").write(src)
+
+print()
+for n in range(15):
+    if not n+1 in check:
+        print(f"[!] Patch {n+1} is not applied")
